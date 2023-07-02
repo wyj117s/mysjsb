995bf7beea710e401dd2f38eb9a6cd13499f5721
岩军 王 <wyj117s@outlook.com>
2023-07-03 00:37:56 +0800

    main

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace opencv6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new main());
         }
     }
 }
